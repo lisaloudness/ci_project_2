2799db1bad7eead6861ef402bbf7c5d0c053b53d
lisaloudness <>
2023-10-14 13:07:01 +0000

    Create dynamic keyboard buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
-console.log("connected")
+const keyboardDiv = document.querySelector(".keyboard");
+const getRandomLogo = () => {
+    
+}
+
+// dynamic keyboard
+for (let i = 97; i <= 122; i++) {
+    const button = document.createElement("button");
+    button.innerText = String.fromCharCode(i);
+    keyboardDiv.appendChild(button);
+}
