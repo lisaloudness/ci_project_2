af5de1567876c654ab0641049673882a70bba361
lisaloudness <>
2023-10-15 13:27:38 +0000

    Script for dynamic keyboard and guessed letters complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,40 @@
+const wordDisplay = document.querySelector(".word-display");
+const guessesText = document.querySelector(".guesses-text b");
 const keyboardDiv = document.querySelector(".keyboard");
-const getRandomLogo = () => {
-    const { question, answer } = logoList[Math.floor(Math.random() * logoList.length)];
-    console.log(question, answer);
+
+let currentWord; wrongGuessCount = 0;
+const maxGuesses = 6;
+
+// retrieve random question from word-list
+const getRandomWord = () => {
+    const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
+    currentWord = word;
+    console.log(word);
+    document.querySelector(".logo-box").src = hint;
+    wordDisplay.innerHTML = word.split("").map(() => `<li class="letter"></li>`).join("");
 }
 
-// dynamic keyboard
+const initGame = (button, clickedLetter) => {
+// check if clickedLetter exist on the currentWord    
+  if(currentWord.includes(clickedLetter)) {
+    [...currentWord].forEach((letter, index) => {
+        if(letter === clickedLetter) {
+            wordDisplay.querySelectorAll("li")[index].innerText = letter;
+            wordDisplay.querySelectorAll("li")[index].classList.add("guessed");
+        }
+    })
+  } else {
+    wrongGuessCount++;
+  }
+  guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
+  }
+
+
+// create dynamic keyboard buttons and click event listener
 for (let i = 97; i <= 122; i++) {
     const button = document.createElement("button");
     button.innerText = String.fromCharCode(i);
     keyboardDiv.appendChild(button);
+    button.addEventListener("click", e => initGame(e.target, String.fromCharCode(i)));
 }
-getRandomLogo();
+getRandomWord();
---
+++ assets/js/word-list.js
@@ -0,0 +1,184 @@
+const wordList = [
+    {
+        word: "atari",
+        hint: "/assets /images /logos.atari.gif"
+    },
+    {  
+        word: "barclays",
+        hint: "/assets/images/logos.barclays.png"
+    },
+    {
+        word: "x",
+        hint: "/assets/images/logos.x.png"
+    },
+    {
+
+        word: "world wildlife fund",
+        hint: "/assets/images/logos.wwf.png"
+    },
+    {
+
+        word: "windows xp",
+        hint: "/assets/images/logos.windows_xp.webp"
+    },
+    {
+
+        word: "wikipedia",
+        hint: "/assets/images/logos/wikipedia.png"
+    },
+    {
+
+        word: "walls",
+        hint: "/assets/images/logos/walls.jpg"
+    },
+    {
+
+        word: "volkswagon",
+        hint: "/assets/images/logos/volkswagon.png"
+    },
+    {
+
+        word: "unilever",
+        hint: "/assets/images/logos/unilever.jpg"
+    },
+    {
+
+        word: "tui",
+        hint: "/assets/images/logos/tui.png"
+    },
+    {
+
+        word: "temple run",
+        hint: "/assets/images/logos/temple_run.png"
+    },
+    {
+
+        word: "starbucks",
+        hint: "/assets/images/logos/starbucks.png"
+    },
+    {
+
+        word: "snapchat",
+        hint: "/assets/images/logos/snapchat.png"
+    },
+    {
+
+        word: "shell",
+        hint: "/assets/images/logos/shell.png"
+    },
+    {
+
+        word: "royal bank of scotland",
+        hint: "/assets/images/logos/royal_bank_of_scotland.png"
+    },
+    {
+
+        word: "roblox",
+        hint: "/assets/images/logos/roblox.jpg"
+    },
+    {
+
+        word: "rolex",
+        hint: "assets/images/logos/rolex.png"
+    },
+    {
+
+        word: "pringles",
+	    hint: "assets/images/logos/pringles.png"
+    },
+    {
+
+        word: "pepsi",
+        hint: "/assets/images/logos/pepsi.webp"
+    },
+    {
+
+        word: "nbc",
+        hint: "/assets/images/logos/NBC.webp"
+    },
+    {
+
+        word: "national trust",
+        hint: "/assets/images/logos/national_trust.png"
+    },
+    {
+
+        word: "motorola",
+        hint: "/assets/images/logos/motorola.png"
+    },
+    {
+
+        word: "micorsoft windows",
+        hint: "/assets/images/logos/micorsoft_windows.jpg"
+    },
+    {
+
+        word: "liverpool fc",
+        hint: "/assets/images/logos/liverpool_fc.png"
+    },
+    {
+
+        word: "kfc",
+        hint: "/assets/images/logos/kfc.webp"
+    },
+    {
+
+        word: "john deere",
+        hint: "/assets/images/logos/john_deere.png"
+    },
+    {
+
+        word: "greggs",
+        hint: "/assets/images/logos/greggs.png"
+    },
+    {
+
+        word: "google chrome",
+        hint: "/assets/images/logos/google_chrome.jpg"
+    },
+    {
+
+        word: "firefox",
+  	hint: "/assets/images/logos/firefox.jpg"
+    },
+    {
+
+        word: "ferrari",
+        hint: "/assets/images/logos/ferrari.jpg"
+    },
+    {
+
+        word: "dominos",
+        hint: "/assets/images/logos/dominos.png"
+    },
+    {
+
+        word: "crocs",
+        hint: "/assets/images/logos/crocs.png"
+    },
+    {
+
+        word: "candy crush",
+        hint: "/assets/images/logos/candy_crush.png"
+    },
+    {
+
+        word: "bp",
+        hint: "/assets/images/logos/BP.webp"
+    },
+    {
+
+        word: "beats",
+        hint: "/assets/images/logos/beats.png"
+    },
+    {
+
+        word: "batman",
+        hint: "assets/images/logos/batman.webp"
+    },
+    {
+
+        word: "barclays",
+        hint: "/assets/images/logos/barclays.png"
+    },
+];
