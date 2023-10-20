2a054b334e85d8026aa9130e44a9e909680f23d7
lisaloudness <>
2023-10-20 10:44:15 +0000

    Activate modal next button & added favicon

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,19 @@ const wordDisplay = document.querySelector(".word-display");
 const guessesText = document.querySelector(".guesses-text b");
 const keyboardDiv = document.querySelector(".keyboard");
 const gameModal = document.querySelector(".game-modal");
+const playAgainBtn = document.querySelector(".play-again");
 
-let currentWord, correctLetters = [], wrongGuessCount = 0;
+let currentWord, correctLetters, wrongGuessCount;
 const maxGuesses = 6;
+
+const resetGame = () => {
+    correctLetters = []
+    wrongGuessCount = 0
+    guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
+    wordDisplay.innerHTML = currentWord.split("").map(() => `<li class="letter"></li>`).join("");
+    keyboardDiv.querySelectorAll("button").forEach(btn => btn.disabled = false);
+    gameModal.classList.remove("show");
+}
 
 // retrieve random question from word-list
 const getRandomWord = () => {
@@ -12,18 +22,19 @@ const getRandomWord = () => {
     currentWord = word;
     console.log(word);
     document.querySelector(".logo").src = hint;
+    resetGame();
     wordDisplay.innerHTML = word.split("").map(() => `<li class="letter"></li>`).join("");
 };
 
 const gameOver = (isVictory) => {
     setTimeout(() => {
-        const modalText = isVictory ?  `You found the word:` : `The correct word was:`;
+        const modalText = isVictory ? `You found the word:` : `The correct word was:`;
         gameModal.querySelector("img").src = `assets/images/${isVictory ? 'victory' : 'lost'}.gif`;
         gameModal.querySelector("h4").innerText = `${isVictory ? 'Well Done!' : 'Unlucky!'}`;
         gameModal.querySelector("p").innerHTML = `${modalText} <b>${currentWord}</b>`;
         gameModal.classList.add("show");
     }, 300);
-} 
+};
 const initGame = (button, clickedLetter) => {
     // check if clickedLetter exist on the currentWord    
     if (currentWord.includes(clickedLetter)) {
@@ -39,7 +50,7 @@ const initGame = (button, clickedLetter) => {
         wrongGuessCount++;
     }
     button.disabled = true;
-    guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;  
+    guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
 
     // gameOver function if these conditions meets
     if (wrongGuessCount === maxGuesses) return gameOver(false);
@@ -56,3 +67,4 @@ for (let i = 97; i <= 122; i++) {
     button.addEventListener("click", e => initGame(e.target, String.fromCharCode(i)));
 }
 getRandomWord();
+playAgainBtn.addEventListener("click", getRandomWord)
