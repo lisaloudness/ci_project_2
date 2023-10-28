0676db730deaa88ebbe4c4b5aaf64a2fd465c1db
lisaloudness <>
2023-10-28 11:40:59 +0000

    Added interactive keyboard-basic functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const startButton = document.getElementById('start-btn');
     const startScreen = document.getElementById('start-screen');
     const playScreen = document.getElementById('play-screen');
-    const progressBar = document.getElementById('progress-bar')
+    const progressBar = document.getElementById('progress-bar');
+    const keyboardDiv = document.querySelector('.keyboard');
 
     let currentWord, correctLetters, wrongGuessCount;
 
@@ -17,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function () {
         progressBar.style.display = "block";
     });
 
-
+//Timer Progress Bar//
     function progress(timeleft, timetotal, $element) {
         var progressBarWidth = timeleft * $element.width() / timetotal;
         $element.find('div').animate({ width: progressBarWidth }, 500).html(Math.floor(timeleft / 60) + ":" + timeleft % 60);
@@ -27,6 +28,16 @@ document.addEventListener("DOMContentLoaded", function () {
             }, 1000);
         }
     };
+    progress(122, 122, $('#progressBar'));
 
-    progress(120, 120, $('#progressBar'));
+
+// create dynamic keyboard buttons and click event listener
+for (let i = 97; i <= 122; i++) {
+    const button = document.createElement("button");
+    button.innerText = String.fromCharCode(i);
+    keyboardDiv.appendChild(button);
+
+    console.log(String.fromCharCode(i));
+};
+
 });
